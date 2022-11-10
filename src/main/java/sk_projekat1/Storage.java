a187e83f9212a58e40e6431f388627fa1485f647
adjenadic <adjenadic11721rn@raf.rs>
2022-11-10 18:59:17 +0100

    Promenjeni argumenti

--- src/main/java/sk_projekat1/Storage.java
+++ src/main/java/sk_projekat1/Storage.java
@@ -1,28 +1,25 @@
 package sk_projekat1;
 
-import sk_projekat1.enums.TypeFilter;
-import sk_projekat1.enums.TypeSort;
-
 import java.util.List;
 
 public interface Storage {
     /**
-     * Check if storage exists.
+     * Checks if the storage exists.
      *
-     * @param apsolutePath  Storage absolute path
-     * @return true or false
+     * @param absolutePath  Storage absolute path
+     * @return              true or false
     */
-    boolean setPath(String apsolutePath);
+    boolean setPath(String absolutePath);
 
     /**
      * Creates an empty root folder called Storage.
      *
-     * @param storageName                 Storage name
-     * @param storageSize                 Storage size in bytes
-     * @param storagePath                 Storage absolute path
-     * @param storageRestrictedExtensions Storage restricted file extensions. If you have multiple extensions, split them with the ',' character
-     * @param maxFilesInFolder            Max number of files in one folder
-     * @return true or false
+     * @param storageName                   Storage name
+     * @param storageSize                   Storage size in bytes
+     * @param storagePath                   Storage absolute path
+     * @param storageRestrictedExtensions   Storage restricted file extensions. If you have multiple extensions, split them with the ',' character
+     * @param maxFilesInFolder              Max number of files in one folder
+     * @return                              true or false
      */
     boolean createStorage(String storageName, String storagePath, int storageSize, String storageRestrictedExtensions, int maxFilesInFolder);
 
@@ -30,7 +27,7 @@ public interface Storage {
      * Creates an empty root folder called 'Default Storage' with default credentials.
      * Default credentials are some constants that are predetermined.
      *
-     * @return true or false
+     * @return                  true or false
      */
     boolean createDefaultStorage();
 
@@ -39,44 +36,44 @@ public interface Storage {
     /**
      * Creates an empty folder.
      *
-     * @param folderName Folder name
-     * @param folderPath Folder path (Relative path from storage)
-     * @return true or false
+     * @param folderName        Folder name
+     * @param folderPath        Folder path (Relative path from storage)
+     * @return                  true or false
      */
     boolean createFolder(String folderName, String folderPath);
 
     /**
      * Creates an empty file.
      *
-     * @param fileName File name
-     * @param filePath File path (Relative path from storage)
-     * @return true or false
+     * @param fileName          File name
+     * @param filePath          File path (Relative path from storage)
+     * @return                  true or false
      */
     boolean createFile(String fileName, String filePath);
 
     /**
      * Moves a file to a given new folder.
      *
-     * @param oldFilePath Old File path (Relative path from storage)
-     * @param newFilePath New File path (Relative path from storage)
-     * @return true or false
+     * @param oldFilePath       Old File path (Relative path from storage)
+     * @param newFilePath       New File path (Relative path from storage)
+     * @return                  true or false
      */
     boolean moveFile(String oldFilePath, String newFilePath);
 
     /**
      * Renames an existing file object.
      *
-     * @param foNewName Folder or File new name
-     * @param foPath    Folder or File path (Relative path from storage)
-     * @return true or false
+     * @param foNewName         Folder or File new name
+     * @param foPath            Folder or File path (Relative path from storage)
+     * @return                  true or false
      */
     boolean renameFileObject(String foNewName, String foPath);
 
     /**
      * Deletes an existing file object.
      *
-     * @param foPath File object path (relative path from storage)
-     * @return true or false
+     * @param foPath            File object path (relative path from storage)
+     * @return                  true or false
      */
     boolean deleteFileObject(String foPath);
 
@@ -85,7 +82,7 @@ public interface Storage {
      *
      * @param importLocalPath   Local disk file path
      * @param importStoragePath Storage file path (Relative path from storage)
-     * @return true or false
+     * @return                  true or false
      */
     boolean importFileObject(String[] importLocalPath, String importStoragePath);
 
@@ -94,7 +91,7 @@ public interface Storage {
      *
      * @param exportStoragePath Storage file path (Relative path from storage)
      * @param exportLocalPath   Local disk file path
-     * @return true or false
+     * @return                  true or false
      */
     boolean exportFileObject(String exportStoragePath, String exportLocalPath);
 
@@ -103,77 +100,77 @@ public interface Storage {
     /**
      * Lists the metadata of all files in a folder.
      *
-     * @param folderPath Folder path (relative path from storage)
-     * @param fileExtension File extension
-     * @param startDate Start date
-     * @param endDate   End date
-     * @param typeSort   Sort type for the file list
-     * @param typeFilter Filter type for the file list
+     * @param folderPath    Folder path (relative path from storage)
+     * @param typeSort      Sort type for the search function
+     * @param typeFilter    Filter type for the search function
+     * @param fileExtension File extension for the search function
+     * @param startDate     Start date
+     * @param endDate       End date
      */
-    List<String> searchFilesInFolder(String folderPath, String fileExtension, String startDate, String endDate, TypeSort typeSort, TypeFilter typeFilter);
+    List<String> searchFilesInFolder(String folderPath, String typeSort, String typeFilter, String fileExtension, String startDate, String endDate);
 
     /**
      * Lists the metadata of all files in a folder, including subfolders.
      *
-     * @param folderPath Folder path (relative path from storage)
-     * @param fileExtension File extension
+     * @param folderPath    Folder path (relative path from storage)
+     * @param typeSort      Sort type for the search function
+     * @param typeFilter    Filter type for the search function
+     * @param fileExtension File extension for the search function
      * @param startDate     Start date
      * @param endDate       End date
-     * @param typeSort   Sort type for the file list
-     * @param typeFilter Filter type for the file list
      */
-    List<String> searchFilesInFolders(String folderPath, String fileExtension, String startDate, String endDate, TypeSort typeSort, TypeFilter typeFilter);
+    List<String> searchFilesInFolders(String folderPath, String typeSort, String typeFilter, String fileExtension, String startDate, String endDate);
 
     /**
      * Lists the metadata of all files with a specific extension in a folder.
      *
-     * @param fileExtension File extension type
      * @param folderPath    Folder path (relative path from storage)
+     * @param typeSort      Sort type for the search function
+     * @param typeFilter    Filter type for the search function
+     * @param fileExtension File extension
      * @param startDate     Start date
      * @param endDate       End date
-     * @param typeSort      Sort type for the file list
-     * @param typeFilter    Filter type for the file list
      */
-    List<String> searchFilesWithExtensionInFolder(String folderPath, String fileExtension, String startDate, String endDate, TypeSort typeSort, TypeFilter typeFilter);
+    List<String> searchFilesWithExtensionInFolder(String folderPath, String typeSort, String typeFilter, String fileExtension, String startDate, String endDate);
 
     /**
      * Lists the metadata of all files with a specific substring in a folder.
      *
-     * @param fileSubstring File substring
      * @param folderPath    Folder path (relative path from storage)
-     * @param fileExtension File extension
+     * @param typeSort      Sort type for the search function
+     * @param typeFilter    Filter type for the search function
+     * @param fileSubstring File substring
+     * @param fileExtension File extension for the search function
      * @param startDate     Start date
      * @param endDate       End date
-     * @param typeSort      Sort type for the file list
-     * @param typeFilter    Filter type for the file list
      */
-    List<String> searchFilesWithSubstringInFolder(String fileSubstring, String folderPath, String fileExtension, String startDate, String endDate, TypeSort typeSort, TypeFilter typeFilter);
+    List<String> searchFilesWithSubstringInFolder(String folderPath, String typeSort, String typeFilter, String fileSubstring, String fileExtension, String startDate, String endDate);
 
     /**
      * Checks if file(s) with the specified name(s) is(/are) in a folder.
      *
-     * @param fileName   Requested name/names files
-     * @param folderPath Folder path (relative path from storage)
-     * @return true or false
+     * @param folderPath    Folder path (relative path from storage)
+     * @param fileName      Requested name/names files
+     * @return              true or false
      */
-    boolean existsInFolder(String[] fileName, String folderPath);
+    boolean existsInFolder(String folderPath, String[] fileName);
 
     /**
      * Lists a folder which includes a file with the specified name.
      *
-     * @param fileName File path (Relative path from storage)
+     * @param fileName      File path (Relative path from storage)
      */
     String findFileFolder(String fileName);
 
     /**
      * Lists the metadata of all files created or modified within a specific date range in a folder.
      *
-     * @param startDate  Start date for the search
-     * @param endDate    End date for the search
-     * @param folderPath Folder path (relative path from storage)
+     * @param folderPath    Folder path (relative path from storage)
+     * @param typeSort      Sort type for the search function
+     * @param typeFilter    Filter type for the search function
+     * @param startDate     Start date for the search function
+     * @param endDate       End date for the search function
      * @param fileExtension File extension
-     * @param typeSort   Sort type for the file list
-     * @param typeFilter Filter type for the file list
      */
-    List<String> searchModifiedFilesInFolder(String folderPath, String fileExtension, String startDate, String endDate, TypeSort typeSort, TypeFilter typeFilter);
+    List<String> searchModifiedFilesInFolder(String folderPath, String typeSort, String typeFilter, String fileExtension, String startDate, String endDate);
 }
